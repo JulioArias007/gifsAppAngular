41d77410f895bc7258b8bfae878455d92a9e9e57
Julio Arias <julio.arias007@gmail.com>
2022-02-22 00:14:53 -0600

    Avance hasta video 89

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+  private apiKey: string = 'diK0uYSmRRapxyABMG1rBU7ARNU87HFs';
+  private _historial: string[] = [];
+  public resultados: Gif[] = [];
+
+  get historial(){
+    return [...this._historial];
+  }
+
+  constructor(private http:HttpClient){
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!)|| [];
+  }
+
+  buscarGifs(query: string){
+    query = query.trim().toUpperCase();
+    
+    if(!this._historial.includes(query)){
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+      
+    }
+
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=diK0uYSmRRapxyABMG1rBU7ARNU87HFs&q=${query}&limit=10`)
+    .subscribe((resp)=>{
+      console.log(resp.data);
+      this.resultados = resp.data;
+      localStorage.setItem('resultados', JSON.stringify(this.resultados));
+    })
+    
+    
+  }
+
+}
